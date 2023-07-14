bef1f6fca0465a37ef7e6a484af442f92d8c2667
benhardtitathiono <113670466+benhardtitathiono@users.noreply.github.com>
2023-07-14 16:33:20 +0700

    nambah picaso, logo error, fun load image

--- app/src/main/java/com/cekepek/ubayakuliner/util/Util.kt
+++ app/src/main/java/com/cekepek/ubayakuliner/util/Util.kt
@@ -1,8 +1,14 @@
 package com.cekepek.ubayakuliner.util
 
 import android.content.Context
+import android.view.View
+import android.widget.ImageView
+import android.widget.ProgressBar
 import androidx.room.Room
+import com.cekepek.ubayakuliner.R
 import com.cekepek.ubayakuliner.model.KulinerDatabase
+import com.squareup.picasso.Picasso
+import com.squareup.picasso.Callback
 
 val DB_NAME = "kulinerdb"
 
@@ -13,4 +19,21 @@ fun buildDb(context: Context):KulinerDatabase {
         .build()
 
     return db
+}
+
+fun ImageView.loadImage(url: String?, progressBar: ProgressBar) {
+    Picasso.get()
+        .load(url)
+        .resize(400, 400)
+        .centerCrop()
+        .error(R.drawable.baseline_error_24)
+        .into(this, object: Callback{
+            override fun onSuccess() {
+                progressBar.visibility= View.GONE
+            }
+
+            override fun onError(e: Exception?) {
+            }
+        })
+
 }
